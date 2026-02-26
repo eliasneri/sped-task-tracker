2bc1c5b0f67ee75cc7ac6ca6bc885c143b061d1a
elias.neri <elias.neri@necbrasil.com.br>
2026-02-25 23:05:27 -0300

    feat: creat TaskService with methods CRUD

---
+++ frontend/src/app/core/services/task-service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {CreateTaskDto, Task, UpdateTaskDto} from '../models/task.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class TaskService {
+    private baseUrl: string = 'http://localhost:5219/api/v1/tasks';
+
+    constructor(private http: HttpClient) {}
+
+  // Busca todas as tasks
+  findAll(): Observable<Task[]> {
+    return this.http.get<Task[]>(`${this.baseUrl}/findall`);
+  }
+
+  findById(id: string): Observable<Task> {
+    return this.http.get<Task>(`${this.baseUrl}/findby/${id}`);
+  }
+
+  create(dto: CreateTaskDto): Observable<Task> {
+    return this.http.post<Task>(`${this.baseUrl}/new`, dto);
+  }
+
+  update(id: string, dto: UpdateTaskDto): Observable<Task> {
+    return this.http.put<Task>(`${this.baseUrl}/update/${id}`, dto);
+  }
+
+  delete(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/delete/${id}`);
+  }
+
+  healthCheck(): Observable<string> {
+    return this.http.get('http://localhost:5219/hc', { responseType: 'text' });
+  }
+}
